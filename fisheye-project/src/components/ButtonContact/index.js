993f9d44f59a4b52952275bfbf9a28438771c18c
Etha <76168237+etharion29@users.noreply.github.com>
2021-08-20 08:59:39 +0200

    Implementation contact modal function

--- fisheye-project/src/components/ButtonContact/index.js
+++ fisheye-project/src/components/ButtonContact/index.js
@@ -1,1 +1,30 @@
 export { default as ButtonContact } from './ButtonContact'
+
+//DOM Elements
+const contactBtn = document.querySelector(".page__button-contact-style");
+
+// launch modal event
+contactBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+contactBtn.addEventListener("click", closeModal);
+
+
+// launch modal form
+function launchModal() {
+    modalbg.style.display = "block";
+  
+    document.querySelectorAll('input[name="location"]').forEach((elem) => {
+      elem.addEventListener("change", function(event) {
+        document.getElementById("alertcity").innerHTML = "";
+      });
+    });
+    
+}
+
+// Close modal form
+function closeModal() {
+    if (modalbg.style.display == "block") {
+      modalbg.style.display = "none";
+    }
+  }
