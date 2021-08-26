e16752214b27c93423a0ba51083346990050b12b
Etha <76168237+etharion29@users.noreply.github.com>
2021-08-26 10:02:03 +0200

    Positionment photos album

--- fisheye-project/src/components/PhotosAlbum/PhotosAlbum.jsx
+++ fisheye-project/src/components/PhotosAlbum/PhotosAlbum.jsx
@@ -7,25 +7,26 @@ import {useParams} from "react-router-dom";
 const PhotosAlbum = () => {
     let { id } = useParams();
     return (
-        <div>
+        <div className="page__album-grid-style">
             {data.media.filter(photo => photo.photographerId == id).map(media => (
-                    <div className="page__album-position">
-                        <img className="page__image-album-style" src={`${process.env.PUBLIC_URL}/asset/photos/Album/${media.image}`} />
-                       {/*  <img className="page__image-album-style"  src={`${process.env.PUBLIC_URL}/asset/photos/Photographers/${photographer.portrait}`} alt="portrait-photographe" /> */}
-                        {/* <div className="page__photo-album-style"><img  src= {photographer.image} alt="portrait-photographe" /></div>
-                        <Link to="/viewAlbum"></Link>
-                        <Router>
-                            <div>
-                            <Route path="/viewAlbum">
-                                <ViewAlbum />
-                            </Route>
-                            </div>
-                        </Router> */}
+                    <div className="page__album-grid-position" >
+                        <div className="page__album-grid">
+                            <img className="page__image-album-style" src={`${process.env.PUBLIC_URL}/asset/photos/Album/${media.image}`} />
+                            {/*  <img className="page__image-album-style"  src={`${process.env.PUBLIC_URL}/asset/photos/Photographers/${photographer.portrait}`} alt="portrait-photographe" /> */}
+                            {/* <div className="page__photo-album-style"><img  src= {photographer.image} alt="portrait-photographe" /></div>
+                            <Link to="/viewAlbum"></Link>
+                            <Router>
+                                <div>
+                                <Route path="/viewAlbum">
+                                    <ViewAlbum />
+                                </Route>
+                                </div>
+                            </Router> */}
+                        </div>
                     </div>
                 ))
             }
         </div>
     )
-    
 }
 export default PhotosAlbum;
