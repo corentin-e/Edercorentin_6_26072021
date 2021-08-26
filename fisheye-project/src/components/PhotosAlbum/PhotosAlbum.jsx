bb089823a63eec1ea5b0dee919295f997b8efc72
Etha <76168237+etharion29@users.noreply.github.com>
2021-08-26 09:50:24 +0200

    Sucess to implemented photographe photo of photographer

--- fisheye-project/src/components/PhotosAlbum/PhotosAlbum.jsx
+++ fisheye-project/src/components/PhotosAlbum/PhotosAlbum.jsx
@@ -8,10 +8,10 @@ const PhotosAlbum = () => {
     let { id } = useParams();
     return (
         <div>
-            {data.photographers.filter(user => user.id == id).map(photographer => (
+            {data.media.filter(photo => photo.photographerId == id).map(media => (
                     <div className="page__album-position">
-                        {/* <img className="page__image-album-style" src={`${process.env.PUBLIC_URL}/asset/photos/Photographer/${photographer.portrait}`} /> */}
-                        <img className="page__image-album-style"  src={`${process.env.PUBLIC_URL}/asset/photos/Photographers/${photographer.portrait}`} alt="portrait-photographe" />
+                        <img className="page__image-album-style" src={`${process.env.PUBLIC_URL}/asset/photos/Album/${media.image}`} />
+                       {/*  <img className="page__image-album-style"  src={`${process.env.PUBLIC_URL}/asset/photos/Photographers/${photographer.portrait}`} alt="portrait-photographe" /> */}
                         {/* <div className="page__photo-album-style"><img  src= {photographer.image} alt="portrait-photographe" /></div>
                         <Link to="/viewAlbum"></Link>
                         <Router>
