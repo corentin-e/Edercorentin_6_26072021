46bb55955ee043f720fdae99a89953bb4ef16366
Etha <76168237+etharion29@users.noreply.github.com>
2021-09-03 08:39:24 +0200

    modif modal

--- fisheye-project/src/components/ButtonContact/ButtonContact.jsx
+++ fisheye-project/src/components/ButtonContact/ButtonContact.jsx
@@ -7,16 +7,10 @@ import React from 'react';
 Modal.setAppElement('#yourAppElement');
 
 const ButtonContact = () => {
-    let subtitle;
     const [modalIsOpen, setIsOpen] = React.useState(false);
   
     function openModal() {
       setIsOpen(true);
-    }
-  
-    function afterOpenModal() {
-      // references are now sync'd and can be accessed.
-      subtitle.style.color = '#f00';
     }
   
     function closeModal() {
@@ -28,7 +22,6 @@ const ButtonContact = () => {
             <button className="page__button-contact-style" onClick={openModal}>Contactez-moi</button>
             <Modal
             isOpen={modalIsOpen}
-            onAfterOpen={afterOpenModal}
             onRequestClose={closeModal}
             contentLabel="Example Modal"
             >
