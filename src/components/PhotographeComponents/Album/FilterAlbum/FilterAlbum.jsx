0417897d5ab73257caaf9795b8690d79fccf66a5
Etha <76168237+etharion29@users.noreply.github.com>
2021-10-16 13:20:26 +0200

    Filter modification finish

--- src/components/PhotographeComponents/Album/FilterAlbum/FilterAlbum.jsx
+++ src/components/PhotographeComponents/Album/FilterAlbum/FilterAlbum.jsx
@@ -2,8 +2,10 @@ import 'react-dropdown/style.css';
 import "./filterAlbum.css"
 import React from 'react';
 import Dropdown from "react-dropdown";
+import Select from "react-select";
 
-const FilterAlbum = ({activeFilter, setActiveFilter}) => {
+const FilterAlbum = ({setActiveFilter}) => {
+
     const options = [
         {value:'popularité', label:'Popularité', className:"options__style",},
         {value:'date', label:'Date', className:"options__style",},
@@ -14,21 +16,26 @@ const FilterAlbum = ({activeFilter, setActiveFilter}) => {
     const changeFilter = (options) => {
         setActiveFilter(options.label);
     };
+
+    const customStyles = {
+        control: (styles) => ({...styles, backgroundColor: 'brown', width: 200, textColor: 'white' }),
+        option: (styles, state) => ({...styles, color: state.isFocused ? "black" : "white" }),
+        singleValue: (styles) => ({...styles, color: 'white'}),
+        placeholder: (styles) => ({...styles, color: 'white'}),
+        indicatorContainer: (styles) => ({...styles, color: 'white'}),
+        menu: (styles) => ({...styles, color: 'white', backgroundColor:'brown'}),
+    }
     
 
     return (
         <div className="page__items-filter-position">
             <label className="page__filter-text">Trier par</label>
-            <div role="button" className="page__filter-dropdown-style">
-                <Dropdown
-                    onChange = {changeFilter}
-                    options={options}
-                    value={defaultOption}
-                    controlClassName='myControlClassName'
-                    arrowClassName='myArrowClassName'
-                    menuClassName='myMenuClassName'
-                />
-            </div>
+            <Select
+                onChange = {changeFilter}
+                options={options}
+                value={defaultOption}
+                styles={customStyles}
+            />
         </div>
     )
     
